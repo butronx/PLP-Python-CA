2cf141fe9fc8fb85029aa3d24bfce389a9e65f99
Emmanuel Madueke <deaconbukat@gmail.com>
2024-03-27 11:11:06 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# This line inquires for the user's name and store it in a variable called "name"
+name = input("What is your name? ")
+
+# This line inquires for the user's age and store it in a variable called "age"
+age = input("How old are you? ")
+
+# This line inquires for the user's location and store it in a variable called "location"
+location = input("Where are you located? ")
+
+# This line Prints out a personalized message using the user's name, age, and location
+print(f"Hello {name}, you are {age} years old and live in {location}.")
